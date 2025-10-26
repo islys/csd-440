ab0242d34f9cc95004e95d5a68c02a11fb7566d7
Ryan Monnier <ryan.w.monnier@gmail.com>
2025-10-26 18:09:51 -0500

    25oct2025

---
+++ module-1/RyanFirstProgram.php
@@ -0,0 +1,20 @@
+<!-- 
+Ryan Monnier
+CSD 440
+Module 1
+26-Oct-2025
+-->
+
+
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
+
+<!-- Script taken from  https://www.w3schools.com/php/ -->
